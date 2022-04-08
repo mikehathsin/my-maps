a4991302a388dfe6ff8a7ad0e250704b8c3dbcbc
Miguel Padrino <me@miguelpadrino.com>
2022-04-08 11:17:13 -0400

    WIP

--- pages/api/location.js
+++ pages/api/location.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export default function handler(req, res) {
-  axios(
-    "https://geolocation-db.com/json/0215bdd0-b516-11ec-b0a9-fdbfeccd28cf"
-  ).then(({ data }) => {
-    res.status(200).json(data);
-  });
+  axios("https://geolocation-db.com/json/0215bdd0-b516-11ec-b0a9-fdbfeccd28cf")
+    .then(({ data }) => {
+      res.status(200).json(data);
+    })
+    .catch(() => {
+      res.status(400).json({ message: "ERROR DE CONEXION AL SERVICIO" });
+    });
 }
